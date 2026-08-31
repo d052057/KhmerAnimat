2cc946b4d5c146eb6636ff4b7cc9456c1ca93f46
Admin <Admin@YPHP>
2026-08-31 15:49:26 -0700

    enhance 1

--- KhmerAnimat.client/src/app/app.routes.ts
+++ KhmerAnimat.client/src/app/app.routes.ts
@@ -2,10 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-  path: 'animate', loadComponent: () => import('./animate/animate')
-    .then(mod => mod.Animate)
-  },
-  {
     path: 'khmerlogo', loadComponent: () => import('./khmer-logo/khmer-logo')
       .then(mod => mod.KhmerLogo)
   }
