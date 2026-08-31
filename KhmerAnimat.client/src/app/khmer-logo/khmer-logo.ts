1042f01856bfd704910f8915ccbb6121006a79a0
Admin <Admin@YPHP>
2026-08-31 00:28:14 -0700

    create animation

--- KhmerAnimat.client/src/app/khmer-logo/khmer-logo.ts
+++ KhmerAnimat.client/src/app/khmer-logo/khmer-logo.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   imports: [],
   selector: 'app-khmer-logo',
   styleUrl: './khmer-logo.scss',
   templateUrl: './khmer-logo.html',
+  encapsulation: ViewEncapsulation.None
 })
 export class KhmerLogo {
 }
